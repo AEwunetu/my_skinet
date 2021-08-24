b35a3fb59f26d5325311c0c273481eaca4fa3e20
Alemayehu Ewunetu <AEwunetu@excellerentsolutions.com>
2021-08-24 14:29:23 +0300

    011 Setting up Entity Framework

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+            
+        }
+    }
+} 
